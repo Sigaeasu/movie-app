baa0daffebe21c49d89f773556f81b9fce99f2eb
Sigi Kemahduta <sigi.kemahduta@edenfarm.id>
2023-05-11 02:25:13 +0700

    remove comment

--- lib/screens/splash/splash.dart
+++ lib/screens/splash/splash.dart
@@ -21,19 +21,12 @@ class SplashView extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocConsumer<AuthCubit, AuthState>(
       listener: (context, state) {
-        // if (state.status == AuthenticationStatus.unAuthenticated) {
-        //   context.read<NotificationCubit>().tokenResetted();
-        // }
+        //
       },
       builder: (context, state) {
-        // Check current authentication state
-        // If user is authenticated, show Home/Main Page
         if (state.status == AuthenticationStatus.authenticated) {
           return MainPage();
-          // return AddMitraPage();
-        }
-        // If user is not authenticated, show login page
-        else if (state.status == AuthenticationStatus.unAuthenticated) {
+        } else if (state.status == AuthenticationStatus.unAuthenticated) {
           return LoginPage();
         }
 
@@ -42,50 +35,3 @@ class SplashView extends StatelessWidget {
     );
   }
 }
-
-// import 'package:flutter/material.dart';
-// import 'package:movieapp/screens/screens.dart';
-
-// class SplashPage extends StatefulWidget {
-//   const SplashPage({Key? key}) : super(key: key);
-
-//   static const routeName = '/';
-
-//   @override
-//   _SplashPageView createState() => _SplashPageView();
-// }
-
-// class _SplashPageView extends State<SplashPage> {
-//   @override
-//   void initState() {
-//     print('masuk');
-//     super.initState();
-//     _navigateHome();
-//   }
-
-//   void _navigateHome() async {
-//     await Future.delayed(Duration(milliseconds: 1500), () {});
-//     print('ada sisini');
-//     Navigator.pushReplacement(
-//         context, MaterialPageRoute(builder: (context) => MainPage()));
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: Center(
-//           child: Container(
-//         child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-//           Icon(Icons.movie, color: Colors.blue[300], size: 150),
-//           Text(
-//             'MovieApp',
-//             style: TextStyle(
-//               fontSize: 40,
-//               color: Colors.blue[300],
-//             ),
-//           )
-//         ]),
-//       )),
-//     );
-//   }
-// }
