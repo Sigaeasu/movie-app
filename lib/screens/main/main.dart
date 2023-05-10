b44908cd8fd0c82b5a8ae986f90905dcbf88e70b
Sigi Kemahduta <sigi.kemahduta@edenfarm.id>
2023-05-10 14:05:45 +0700

    init screens and main screen

---
+++ lib/screens/main/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  static const routeName = '/';
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Movie App"),
+      ),
+      body: Text('Movie App'),
+    );
+  }
+}
